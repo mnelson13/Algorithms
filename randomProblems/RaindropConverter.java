d2058f72d03e1b2c64ea89afaceb513820f0d75c
MNelson <melissa.nelson13@yahoo.com>
2019-01-16 00:22:23 -0600

    solved first Java problem

---
+++ randomProblems/RaindropConverter.java
@@ -0,0 +1,44 @@
+/*
+Problem:
+    Convert a number to a string, the contents of which depend on the number's factors.
+
+If the number has 3 as a factor, output 'Pling'.
+If the number has 5 as a factor, output 'Plang'.
+If the number has 7 as a factor, output 'Plong'.
+If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+
+*/
+
+class RaindropConverter {
+
+    public static void main(String args[]) {
+        convert(1); 
+        convert(3);
+        convert(5); 
+        convert(7); 
+        convert(105);  
+    }
+
+    public static String convert(int number) {
+        String str = "";
+        if (number % 3 == 0) {
+            str += "Pling";
+        }
+        if (number % 5 == 0) {
+            str += "Plang";
+        }
+        if (number % 7 == 0) {
+            str += "Plong";
+        }
+        if (str == "") {
+            str += number;
+        }
+
+        System.out.println(str);
+
+        return str;
+    }
+
+}
+
+
